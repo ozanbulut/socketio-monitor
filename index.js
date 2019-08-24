43b6e58c48877f44ea7f09c1f2f1f37a8b626019
ozanbulut <ozanbulut1994@gmail.com>
2019-08-24 10:25:36 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+app.post('/data', function(req, res){
+	var body = "";
+    req.on("data", function (data) {
+        body += data;
+    });
+    req.on("end", function() {
+	io.emit('chat message', body);
+        console.log(JSON.parse(body));
+        res.json({ message: 'goodbye'})
+    });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
